a6fc36b1773482cbc29dd2d4f52631528b29eff3
Ben Browner <bnbrowner@gmail.com>
2021-12-05 00:21:58 -0500

    Day 4

---
+++ day4/part1/d4p1.go
@@ -0,0 +1,112 @@
+// generated at 2021-12-04 16:23:05.369222+00:00, for day 4 part 1
+package main
+
+// issues with board parsing, all fixed now
+
+import (
+    "advent-of-code-2021/utils"
+    "bufio"
+    "fmt"
+    "strconv"
+    "strings"
+)
+
+// negative values means selected
+type Board [5][5]int
+
+func (b *Board) String() string {
+    var sb strings.Builder
+    for _, r := range b {
+        fmt.Fprintf(&sb, "%v\n", r)
+    }
+    return sb.String()
+}
+
+func (b *Board) HasPogMoment() bool {
+    // rows
+    for _, r := range b {
+        if r[0] < 0 && r[1] < 0 && r[2] < 0 && r[3] < 0 && r[4] < 0 {
+            return true
+        }
+    }
+
+    // cols
+    for i := 0; i < 5; i++ {
+        if b[0][i] < 0 && b[1][i] < 0 && b[2][i] < 0 && b[3][i] < 0 && b[4][i] < 0 {
+            return true
+        }
+    }
+
+    return false
+}
+
+func (b *Board) Sum() int {
+    sum := 0
+    for r := 0; r < 5; r++ {
+        for c := 0; c < 5; c++ {
+            if b[r][c] > 0 {
+                sum += b[r][c]
+            }
+        }
+    }
+    return sum
+}
+
+func NewBoard(s *bufio.Scanner, spots *[100][]*int) *Board {
+    b := &Board{}
+    r := 0
+    for r < 5 && s.Scan() {
+        for c, val := range strings.Fields(s.Text()) {
+            fmt.Sscanf(val, "%d", &b[r][c])
+            spot := &spots[b[r][c]]
+            *spot = append(*spot, &b[r][c])
+        }
+
+        r++
+    }
+    return b
+}
+
+func main() {
+    scanner, err := utils.LoadScanner("data/day4.txt")
+    if err != nil { panic(err) }
+
+    inputs := make([]int, 0)
+    boards := make([]*Board, 0)
+    var spots [100][]*int
+
+    for i := range spots {
+        spots[i] = make([]*int, 0)
+    }
+
+    // parse input
+    for _, val := range strings.Split(utils.Next(scanner), ",") {
+        num, err := strconv.Atoi(val)
+        if err != nil {
+            panic(err)
+        }
+        inputs = append(inputs, num)
+    }
+
+    // parse boards
+    for scanner.Scan() {
+        boards = append(boards, NewBoard(scanner, &spots))
+    }
+
+
+    // apply inputs and check for winners
+    outer: for _, input := range inputs {
+        for _, spot := range spots[input] {
+            *spot = -1
+        }
+
+        for _, board := range boards {
+            if board.HasPogMoment() {
+                fmt.Println(board)
+                fmt.Println(board.Sum() * input)
+                break outer
+            }
+        }
+    }
+}
+
